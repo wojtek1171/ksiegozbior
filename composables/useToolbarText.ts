b4ba0839f347e297ceb206eb5fede95ef4fd3a4f
wojtek <f.janek@o2.pl>
2023-08-04 00:25:59 +0200

    remove title

--- composables/useToolbarText.ts
+++ composables/useToolbarText.ts
@@ -1,7 +1,5 @@
 export default function useToolbarText() {
   const toolbarText = ref('');
-  console.log('use');
-  console.log(toolbarText.value);
 
   function changeText(text) {
     toolbarText.value = text;
