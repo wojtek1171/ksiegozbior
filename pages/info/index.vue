15cdf85c0909273fada808d8e6228c1b3588a71f
wojtek <f.janek@o2.pl>
2023-08-12 12:49:32 +0200

    add logos in infoPage

--- pages/info/index.vue
+++ pages/info/index.vue
@@ -1,3 +1,16 @@
 <template>
-  <div class="text-h4">Info strona</div>
+  <q-page class="q-pa-xl for-page">
+    <div class="q-my-lg text-h4 text-center">W projekcie wykorzystano:</div>
+    <div class="row flex items-center justify-center q-gutter-lg">
+      <div>
+        <q-img src="~/assets/info-page/nuxt-logo.png" width="250px"></q-img>
+      </div>
+      <div>
+        <q-img src="~/assets/info-page/vue-logo.png" width="200px"></q-img>
+      </div>
+    </div>
+    <div class="q-mt-lg flex items-center justify-center">
+      <q-img src="~/assets/info-page/mongo-logo.png" width="500px"></q-img>
+    </div>
+  </q-page>
 </template>
