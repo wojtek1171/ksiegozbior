e4cc715ced00993b3f20691e78472de3f9eb0cba
wojtek <f.janek@o2.pl>
2023-08-24 16:31:27 +0200

    add guid to RSS item and cache 60 min

--- server/routes/quotes/rss.ts
+++ server/routes/quotes/rss.ts
@@ -4,21 +4,23 @@ export default defineEventHandler(async (event) => {
   try {
     const quotes = await $fetch('/api/quotes/by_creation_desc_recent');
 
-    // if (!quotes.ok) {
-    //   throw new Error(quotes?.status);
-    // }
+    if (!quotes) {
+      throw new Error('Something went wrong');
+    }
 
     const feed = new RSS({
       title: 'Cytaty',
       site_url: 'https://ksiegozbior.vercel.app',
       feed_url: `https://ksiegozbior.vercel.app/quotes/rss`,
+      ttl: 60,
     });
 
     for (const quote of quotes) {
       feed.item({
         title: `${quote.bookTitle} - ${quote.authors}` || 'title',
         url: 'https://ksiegozbior.vercel.app/quotes',
-        description: quote.text,
+        guid: quote._id,
+        description: `<p>${quote.text.replaceAll('\r\n', '<br>')}</p>`,
         date: quote.created,
         categories: quote.tags.split(','),
       });
