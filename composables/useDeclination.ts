3a5eabc27b655f8fbd077014cd813e30e4ddb5a2
wojtek <f.janek@o2.pl>
2023-09-08 20:37:57 +0200

    fix declination

--- composables/useDeclination.ts
+++ composables/useDeclination.ts
@@ -2,6 +2,8 @@ export default function useDeclination() {
   function declineBook(booksCount) {
     if (booksCount === 1) {
       return 'książka';
+    } else if (booksCount % 100 > 10 && booksCount % 100 < 20) {
+      return 'książek';
     } else if (booksCount % 10 === 2 || booksCount % 10 === 3 || booksCount % 10 === 4) {
       return 'książki';
     }
@@ -11,6 +13,8 @@ export default function useDeclination() {
   function declineQuote(quotesCount) {
     if (quotesCount === 1) {
       return 'cytat';
+    } else if (quotesCount % 100 > 10 && quotesCount % 100 < 20) {
+      return 'cytatów';
     } else if (quotesCount % 10 === 2 || quotesCount % 10 === 3 || quotesCount % 10 === 4) {
       return 'cytaty';
     }
@@ -20,6 +24,8 @@ export default function useDeclination() {
   function declinePage(pagesCount) {
     if (pagesCount === 1) {
       return 'stronę';
+    } else if (pagesCount % 100 > 10 && pagesCount % 100 < 20) {
+      return 'stron';
     } else if (pagesCount % 10 === 2 || pagesCount % 10 === 3 || pagesCount % 10 === 4) {
       return 'strony';
     }
@@ -36,6 +42,8 @@ export default function useDeclination() {
   function declineShelves(shelvesCount) {
     if (shelvesCount === 1) {
       return 'półkę';
+    } else if (shelvesCount % 100 > 10 && shelvesCount % 100 < 20) {
+      return 'półek';
     } else if (shelvesCount % 10 === 2 || shelvesCount % 10 === 3 || shelvesCount % 10 === 4) {
       return 'półki';
     }
