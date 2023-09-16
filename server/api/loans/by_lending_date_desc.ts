a6a82e7f0d1b775b4e80a98fa1fb3261688c5b5d
wojtek <f.janek@o2.pl>
2023-09-16 10:12:34 +0200

    change loans sorting

--- server/api/loans/by_lending_date_desc.ts
+++ server/api/loans/by_lending_date_desc.ts
@@ -1,7 +1,7 @@
 import { Loan } from '~/server/models/loan.schema';
 
 export default defineEventHandler(async (event) => {
-  const loanes = await Loan.find().sort({ lendingDate: 1 });
+  const loanes = await Loan.find().sort({ lendingDate: -1 });
 
   return loanes;
 });
